50b681911b203df0ed6f3b477a54b49562529686
Professor IFPR <professor@ifpr.edu.br>
2023-03-30 22:52:22 -0300

    Encapsulamento

---
+++ src/br/ifpr/aula/pacotes/JogadorVolei.java
@@ -0,0 +1,17 @@
+package br.ifpr.aula.pacotes;
+
+public class JogadorVolei {
+    private String corCabelo;
+    String corPele;
+    public String nome;
+    protected String sobrenome;
+
+    private String getCorCabelo(){
+        return this.corCabelo;
+    }
+    
+    public void setCorCabelo(String corCabelo){
+        this.corCabelo = corCabelo;
+        
+    }
+}
---
+++ src/br/ifpr/aula/pacotes/Teste.java
@@ -0,0 +1,12 @@
+package br.ifpr.aula.pacotes;
+
+public class Teste {
+    public static void main(String[] args) {
+        JogadorVolei simaoPerdedor = new JogadorVolei();
+
+        //String corCabelo = simaoPerdedor.getCorCabelo();
+        simaoPerdedor.setCorCabelo("Azul");
+
+    }
+    
+}
